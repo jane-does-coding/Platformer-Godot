ffebb123d67fbf1c0e2edda4a7e2f3dc32f2c82f
Jane <yevheniiasimaka@gmail.com>
2026-01-25 18:37:01 -0700

    making it into my own maze game

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,11 +1,9 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("I am a coin.")
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
+	animation_player.play("pickup")
